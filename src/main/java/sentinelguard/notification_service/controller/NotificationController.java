4955148eee77e7b0923f6a9a30606a7884c2323d
Anton Rudnikovskiy <mkasadasa@yandex.ru>
2024-06-09 15:57:49 +0300

    removed import

--- src/main/java/sentinelguard/notification_service/controller/NotificationController.java
+++ src/main/java/sentinelguard/notification_service/controller/NotificationController.java
@@ -1,6 +1,5 @@
 package sentinelguard.notification_service.controller;
 
-import io.micrometer.observation.annotation.Observed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
